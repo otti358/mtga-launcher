5d1eca6e7535fdaa15cafe4102674fd159ea5927
otty <lukas.schaal@carmaware.io>
2020-08-13 00:56:53 +0200

    Update

--- update.sh
+++ update.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
-# Download the latest Magic version **[here](https://mtgarena.downloads.wizards.com/Live/Windows32/version)**
-# Copy the downloaded file into the installation folder  ```$HOME/.local/apps/magic/```
-# Change file name in script ```update.sh``` and execute.
-
+# Download the latest MTGA version here
+# Copy the MTGAINSTALLER.EXE into the installation directory $HOME/.local/apps/magic/
+# Rename folder-paths in script nano update.sh and execute ./update.sh
 
 ## Update
 
